a20ac993ffcf6d3dd68dab1d65e9d0e749a81884
DigitalEternities <maxim3950054@gmail.com>
2025-08-01 18:49:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from core.run import main
 if __name__ == "__main__":
     try:
         print(colored("=== Запуск RUX UserBot ===", "blue"))
+        os.system('termux-toast "RUX запускается..."')
         asyncio.run(main())
     except KeyboardInterrupt:
         print(colored("\nrux завершил работу", "yellow"))
